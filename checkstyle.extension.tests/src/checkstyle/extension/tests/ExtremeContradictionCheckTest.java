c6dde668b14a27132ab5e21c73c8f4412aee43f6
Nikolas Tokuno <nikolas.tokuno@wsu.edu>
2017-09-28 15:59:01 -0700

    Added test project for testing the implemented checks

---
+++ checkstyle.extension.tests/src/checkstyle/extension/tests/ExtremeContradictionCheckTest.java
@@ -0,0 +1,59 @@
+package checkstyle.extension.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ExtremeContradictionCheckTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetDefaultTokens() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEnglishDictionary() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetEnglishDictionary() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetAllowedAbbreviationLength() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVisitTokenDetailAST() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsJavaKeyword() {
+		fail("Not yet implemented");
+	}
+
+}
