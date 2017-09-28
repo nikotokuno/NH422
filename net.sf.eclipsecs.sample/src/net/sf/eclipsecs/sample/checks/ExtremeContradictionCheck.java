c4c062fb4dde081b32fb3aea4826fbeaaf578a49
Nikolas Tokuno <nikolas.tokuno@wsu.edu>
2017-09-28 15:21:09 -0700

    Began ExtrameContradictionCheck in the Sample Project

---
+++ net.sf.eclipsecs.sample/src/net/sf/eclipsecs/sample/checks/ExtremeContradictionCheck.java
@@ -0,0 +1,156 @@
+package net.sf.eclipsecs.sample.checks;
+
+import java.util.*;
+
+import com.puppycrawl.tools.checkstyle.api.AbstractCheck;
+import com.puppycrawl.tools.checkstyle.api.DetailAST;
+import com.puppycrawl.tools.checkstyle.api.TokenTypes;
+
+/**
+ * @author nikot
+ *
+ * 	Extreme contraction: refers to extremely short terms
+ *	used in identifiers due to an excessive word contraction,
+ *	abbreviation, or acronym. An example of such identifier is
+ *	aSz (a=array, sz=size). 
+ *
+ *	This rule does not apply to prefixes
+ *	introduced due to the naming conventions adopted in the
+ *	system (e.g., m is a prefix used in the Hungarian notation to
+ *	mark attributes of a class), common programming and domain
+ *	terms (e.g., msg, SQL, etc.), and short dictionary words (e.g.,
+ *	on, it, etc.).
+ *
+ *
+ */
+public class ExtremeContradictionCheck extends AbstractCheck {
+	
+    /**
+     * Warning message key.
+     */
+    public static final String MESSAGE_KEY = "abbreviation.as.word";
+	
+	/**
+     * Set of allowed English words.
+     */
+	private HashSet<String> englishDictionary;
+	
+	/**
+     * Set of allowed abbreviation to ignore in check.
+     */
+    private Set<String> allowedAbbreviations = new HashSet<>();
+    
+    /**
+     * Variable indicates on the allowed amount of letters in
+     * in identifiers to check for. Default is 3. 
+     */
+    private int allowedAbbreviationLength = 3;
+	
+	/**
+	 * @return the englishDictionary
+	 */
+	public HashSet<String> getEnglishDictionary() {
+		return englishDictionary;
+	}
+
+	/**
+	 * @param englishDictionary the englishDictionary to set
+	 */
+	public void setEnglishDictionary() {
+		this.englishDictionary = readDictionaryWordsFromFile();
+	}
+	
+	private HashSet<String> readDictionaryWordsFromFile() {
+		HashSet<String> dictionary = new HashSet<String>();
+		
+		return dictionary;
+	}
+	
+    /**
+     * Allowed abbreviation length in names.
+     * @param allowedAbbreviationLength
+     *            amount of allowed capital letters in abbreviation.
+     */
+    public void setAllowedAbbreviationLength(int allowedAbbreviationLength) {
+        this.allowedAbbreviationLength = allowedAbbreviationLength;
+    }
+
+	@Override
+	public int[] getDefaultTokens() {
+		 return new int[] {
+		            TokenTypes.CLASS_DEF,
+		            TokenTypes.INTERFACE_DEF,
+		            TokenTypes.ENUM_DEF,
+		            TokenTypes.ANNOTATION_DEF,
+		            TokenTypes.ANNOTATION_FIELD_DEF,
+		            TokenTypes.PARAMETER_DEF,
+		            TokenTypes.VARIABLE_DEF,
+		            TokenTypes.METHOD_DEF,
+		        };
+	}
+
+
+	 @Override
+	 public void visitToken(DetailAST ast) {
+		 
+		 if (!isIgnoreSituation(ast)) {
+
+	            final DetailAST nameAst = ast.findFirstToken(TokenTypes.IDENT);
+	            final String typeName = nameAst.getText();
+	            
+	            log(nameAst.getLineNo(), MESSAGE_KEY, typeName, 
+	                		allowedAbbreviationLength + 1);
+	            }
+	        }
+	  }
+	
+	 
+	    /**
+	     * Checks if it is an ignore situation.
+	     * @param ast input DetailAST node.
+	     * @return true if it is an ignore situation found for given input DetailAST
+	     *         node.
+	     */
+	    private boolean isIgnoreSituation(DetailAST ast) {
+	    	final DetailAST nameAst = ast.findFirstToken(TokenTypes.IDENT);
+            final String typeName = nameAst.getText();
+
+	        final boolean result;
+	        
+	        // Ignore if the identifier is in the English dictionary i.e. it, on
+	        if (englishDictionary.add(typeName)) {
+	            result = true;
+	        }
+	        
+	        // Ignore if 
+	        else if (allowedAbbreviations.contains(typeName)) {
+	            result = true;
+	        }
+	        else {
+	            result = CheckUtils.isReceiverParameter(ast);
+	        }
+	        return result;
+	    }
+	 
+	 
+	 
+	 
+
+	    /**
+	     * Gets all the children which are one level below on the current DetailAST
+	     * parent node.
+	     * @param node
+	     *        Current parent node.
+	     * @return The list of children one level below on the current parent node.
+	     */
+	    private static List<DetailAST> getChildren(final DetailAST node) {
+	        final List<DetailAST> result = new LinkedList<>();
+	        DetailAST curNode = node.getFirstChild();
+	        while (curNode != null) {
+	            result.add(curNode);
+	            curNode = curNode.getNextSibling();
+	        }
+	        return result;
+	    }
+
+}
