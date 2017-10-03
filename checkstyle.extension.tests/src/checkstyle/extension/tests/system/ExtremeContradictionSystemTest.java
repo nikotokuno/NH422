9a01843bcf872e202677f70ddc36781aa135c9d1
Nikolas Tokuno <nikolas.tokuno@wsu.edu>
2017-10-03 11:37:03 -0700

    Added syustem tests for ExtremeContradictionCheck

---
+++ checkstyle.extension.tests/src/checkstyle/extension/tests/system/ExtremeContradictionSystemTest.java
@@ -0,0 +1,52 @@
+package checkstyle.extension.tests.system;
+
+import static org.junit.Assert.*;
+
+/* System Test:
+ * Functional - 
+ * Performance -
+ * Stress -
+ * Config -
+ * Security -
+ * Realiability - 
+ * Usability - 
+ * 
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+/* Functional Reuirements:
+ * 
+ * 
+ * 
+ */
+
+public class ExtremeContradictionSystemTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
